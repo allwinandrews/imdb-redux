767a2a2f86becb3e2f956126d9ac0ea538a04ba7
allwinjandrews <allwin@labglo.com>
2019-12-18 12:48:38 +0530

    basic react modules

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Provider } from "react-redux";
+import { HashRouter as Router, Route } from "react-router-dom";
+
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+import Landing from "./components/home/Landing";
+import Movie from "./components/home/Movie";
+import store from "./store";
+
+function App() {
+  return (
+    <Provider store={store}>
+      <Router>
+        <Navbar />
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/movie/:id" component={Movie} />
+        <Footer />
+      </Router>
+    </Provider>
+  );
+}
+
+export default App;
---
+++ src/components/layout/Footer.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div>
+      <div className="row">
+        <div className="col-md-12">
+          <div className="footer p-3 mt-4 text-center bg-dark text-light">
+            <a
+              href="http://www.omdbapi.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              OMDB
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
